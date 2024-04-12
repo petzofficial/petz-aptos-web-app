f08e99e9cc23ea0f4095db1a1b3ca18cf9ed71e1
hammadkhanwazir <insafstudentpti@gmail.com>
2024-04-12 20:58:15 +0500

    terms and condition updated

---
+++ src/utils/privacy/terms.js
@@ -0,0 +1,61 @@
+export const termsAndConditions = [
+  {
+    section: "ACCEPTANCE OF TERMS",
+    content:
+      "These Terms constitute a legally binding agreement between you and Petz Money. By accessing or using our dApp, you acknowledge that you have read, understood, and agreed to be bound by these Terms.",
+  },
+  {
+    section: "ELIGIBILITY",
+    content:
+      "Our dApp is intended solely for users who are legally permitted to engage in decentralized finance (DeFi) activities and use blockchain-based applications. You represent and warrant that you are of legal age and have the capacity to enter into these Terms.",
+  },
+  {
+    section: "PLATFORM USAGE",
+    content:
+      "You agree to use our dApp solely for lawful purposes and in compliance with all applicable laws and regulations. You are solely responsible for your actions and interactions while using our dApp, including but not limited to any transactions, smart contract interactions, and transfers of digital assets.",
+  },
+  {
+    section: "RISK ACKNOWLEDGMENT",
+    content:
+      "You acknowledge and understand that the use of decentralized applications, smart contracts, and blockchain technology carries inherent risks, including but not limited to technical risks, regulatory risks, and financial risks. You are solely responsible for evaluating and understanding the risks associated with your use of our dApp.",
+  },
+  {
+    section: "NO FINANCIAL ADVICE",
+    content:
+      "Our dApp and any associated materials are provided for informational purposes only and do not constitute financial advice. We do not recommend or endorse any specific digital assets, investment strategies, or financial products.",
+  },
+  {
+    section: "INTELLECTUAL PROPERTY",
+    content:
+      "All intellectual property rights, including but not limited to copyrights, trademarks, and trade secrets, related to our dApp and its associated materials are owned by Petz Money or its licensors. You may not reproduce, modify, distribute, or create derivative works based on our dApp or its associated materials without our prior written consent.",
+  },
+  {
+    section: "DISCLAIMER OF WARRANTIES",
+    content:
+      "Our dApp is provided on an 'as is' and 'as available' basis, without any warranties of any kind, whether express or implied, including but not limited to warranties of merchantability, fitness for a particular purpose, and non-infringement.",
+  },
+  {
+    section: "LIMITATION OF LIABILITY",
+    content:
+      "In no event shall Petz Money, its affiliates, or its partners be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to damages for loss of profits, goodwill, use, data, or other intangible losses, resulting from your use or inability to use our dApp, or any associated products or services.",
+  },
+  {
+    section: "INDEMNIFICATION",
+    content:
+      "You agree to indemnify, defend, and hold harmless Petz Money, its affiliates, and its partners from and against any and all claims, liabilities, damages, losses, and expenses, including reasonable attorneys' fees, arising from your use of our dApp or your violation of these Terms.",
+  },
+  {
+    section: "MODIFICATION OF TERMS",
+    content:
+      "We reserve the right to modify or update these Terms at any time without prior notice. Your continued use of our dApp after any modifications or updates constitutes your acceptance of the revised Terms.",
+  },
+  {
+    section: "Contact",
+    content:
+      "If you have any questions or concerns regarding these Terms and Conditions, please contact us at petz.money@gmail.com.",
+  },
+  {
+    section: "Effective date",
+    content: "30/3/2024",
+  },
+];
